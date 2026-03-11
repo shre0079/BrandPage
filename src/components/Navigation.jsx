1a2cb0123d1521f79e51df2923bd8c56e9e5f613
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-03-12 00:41:49 +0530

    navigation pane component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,27 @@
+const Navigation = () => {
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="logo" />
+      </div>
+      <ul>
+        <li>
+          <a href="#">Menu</a>
+        </li>
+        <li>
+          <a href="#">Location</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+
+      <button>login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
